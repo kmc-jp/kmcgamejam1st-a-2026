73a2ce65ddf13787d730cb21f7c02cbf1d99cc11
biuntri <biuntri.213@gmail.com>
2026-03-11 16:27:25 +0900

    ScoreIndicatorを作成(エラーあり)

---
+++ Assets/Scenes/gameScene/ScoreIndicator/ScoreIndicatorCon.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+
+public class ScoreIndicatorCon : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI TMP;
+	[SerializeField] GameManager Manager;
+
+
+
+    public void ShowScore()
+	{
+		 Manager.Score.Subscribe(score => scoreUI.text = $"score").AddTo(this);
+	}
+
+	public void Reset()
+	{
+		
+	}
+}
