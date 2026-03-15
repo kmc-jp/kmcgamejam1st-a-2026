a6961a62404cb4bb1c7f295c61d1c768397ccf78
wasabi-eater <yusuke.akinari@gmail.com>
2026-03-15 17:43:13 +0900

    BGMを追加

---
+++ Assets/Scripts/BGMManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class BGMManager : MonoBehaviour
+{
+    AudioSource audioSource;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
+    }
+}
